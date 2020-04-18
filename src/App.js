2d6712f6e196d47256eda3db3e6d83b42a57327f
Daryl Bacon <github@darylbacon.co.uk>
2020-04-18 11:40:41 +0100

    make component reuseable by passing props and outputting them dynamically

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person />
-        <Person />
-        <Person />
+        <Person name="Daryl" age="25">My Hobbies: Racing</Person>
+        <Person name="James" age="28" />
+        <Person name="Percy" age="29" />
       </div>
     )
   }
