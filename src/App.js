b591a702839a583c8fba4bfbb488a0f1d8390150
Daryl Bacon <github@darylbacon.co.uk>
2020-04-18 12:01:23 +0100

    use state to store persons and pass that to the persons component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,32 @@ import './App.css'
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      {
+        name: 'Daryl',
+        age: 25
+      },
+      {
+        name: 'Jame',
+        age: 28
+      },
+      {
+        name: 'Percy',
+        age: 29
+      },
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person name="Daryl" age="25">My Hobbies: Racing</Person>
-        <Person name="James" age="28" />
-        <Person name="Percy" age="29" />
+        <button>Switch Name</button>
+        <Person name={ this.state.persons[0].name } age={ this.state.persons[0].age }>My Hobbies: Racing</Person>
+        <Person name={ this.state.persons[1].name } age={ this.state.persons[1].age } />
+        <Person name={ this.state.persons[2].name } age={ this.state.persons[2].age } />
       </div>
     )
   }
