00d192c82c5b36109f9adf1c70ae8d82ccedbbbb
Daryl Bacon <github@darylbacon.co.uk>
2020-04-19 15:24:36 +0100

    convert to the useState() hook :: use array destructuring to set the state and a function to update it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
+const app = () => {
+  const [ personsState, setPersonsState ] = useState({
     persons: [
       {
         name: 'Daryl',
@@ -17,13 +17,13 @@ class App extends Component {
         name: 'Percy',
         age: 29
       },
-    ],
-    otherState: 'some value'
-  }
+    ]
+  })
 
-  switchNameHandler = () => {
-    // console.log('was clicked')
-    this.setState({
+  const [ otherState, setOtherState] = useState('some value')
+
+  const switchNameHandler = () => {
+    setPersonsState({
       persons: [
         {
           name: 'Daryl',
@@ -41,18 +41,16 @@ class App extends Component {
     })
   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button onClick={ this.switchNameHandler }>Switch Name</button>
-        <Person name={ this.state.persons[0].name } age={ this.state.persons[0].age }>My Hobbies: Racing</Person>
-        <Person name={ this.state.persons[1].name } age={ this.state.persons[1].age } />
-        <Person name={ this.state.persons[2].name } age={ this.state.persons[2].age } />
-      </div>
-    )
-  }
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <p>This is really working!</p>
+      <button onClick={ switchNameHandler }>Switch Name</button>
+      <Person name={ personsState.persons[0].name } age={ personsState.persons[0].age }>My Hobbies: Racing</Person>
+      <Person name={ personsState.persons[1].name } age={ personsState.persons[1].age } />
+      <Person name={ personsState.persons[2].name } age={ personsState.persons[2].age } />
+    </div>
+  )
 }
 
-export default App;
+export default app;
