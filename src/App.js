c9c8625008bc2e36ee7b12a22a76d49255396044
Daryl Bacon <github@darylbacon.co.uk>
2020-05-19 08:22:04 +0100

    adding dynamic styles

--- src/App.js
+++ src/App.js
@@ -62,7 +62,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: ' 1px solid blue',
       padding: '8px',
@@ -87,6 +88,8 @@ class App extends Component {
           })}
         </div>
       )
+
+      style.backgroundColor = 'red'
     }
 
     return (
