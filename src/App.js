e685bf915fc452f660d7ba14e778975f4bc39e28
Daryl Bacon <github@darylbacon.co.uk>
2020-05-21 08:29:21 +0100

    working with css modules :: modifying css to use css modules :: adding media queries with css modules

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import './App.css'
+import classes from './App.css'
 import Person from './Person/Person'
 
 class App extends Component {
@@ -62,7 +62,7 @@ class App extends Component {
   }
 
   render() {
-
+    let btnClass = ''
     let persons = null
 
     if (this.state.showPersons) {
@@ -81,23 +81,25 @@ class App extends Component {
           })}
         </div>
       )
+
+      btnClass = classes.Red
     }
 
-    const classes = []
+    const assignedClasses = []
 
     if (this.state.persons.length <= 2) {
-      classes.push('red')
+      assignedClasses.push(classes.red)
     }
 
     if (this.state.persons.length <= 1) {
-      classes.push('bold')
+      assignedClasses.push(classes.bold)
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button className="button"
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
+        <button className={btnClass}
           alt={this.state.showPersons}
           onClick={this.togglePersonsHandler}>
             Toggle Persons
