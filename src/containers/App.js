d9d15d2c0ceb63a5b4b987bf933d2c626d977b1e
Daryl Bacon <github@darylbacon.co.uk>
2020-05-28 08:42:57 +0100

    component update lifecycle - for state chnages

--- src/containers/App.js
+++ src/containers/App.js
@@ -41,6 +41,15 @@ class App extends Component {
     console.log('[App.js] componentDidMount')
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate')
+    return true
+  }
+
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate')
+  }
+
   deletePersonHandler = (personIndex) => {
     const persons = [...this.state.persons]
 
