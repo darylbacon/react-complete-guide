855d45764dfc59d9c668fedd9929ae1c54f30332
Daryl Bacon <github@darylbacon.co.uk>
2020-05-28 08:15:55 +0100

    component creation lifecycle

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,11 @@ import Persons from '../components/Persons/Persons'
 import Cockpit from '../components/Cockpit/Cockpit'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    console.log('[App.js] constructor')
+  }
+
   state = {
     persons: [
       {
@@ -25,6 +30,15 @@ class App extends Component {
     ],
     otherState: 'some value',
     showPersons: false
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props)
+    return state
+  }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount')
   }
 
   deletePersonHandler = (personIndex) => {
@@ -63,6 +77,7 @@ class App extends Component {
   }
 
   render() {
+    console.log('[App.js] render')
     let persons = null
 
     if (this.state.showPersons) {
