db30d3ab90a3e2e20f9d6a2d89bd177fbe30b815
Daryl Bacon <github@darylbacon.co.uk>
2020-05-29 08:12:11 +0100

    using useEffect() in functional components :: this is componentDidMount and componenetDidUpdate combined into one hook :: pass props.person as second parameter so it only runs on initial load and then only when a person is changed

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import classes from './Cockpit.css'
 
 const cockpit = (props) => {
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect')
+    // Http request...
+    setTimeout(() => {
+      alert('Saved data to cloud')
+    }, 1000)
+  }, [props.persons])
+
   const assignedClasses = []
   let btnClass = ''
 
